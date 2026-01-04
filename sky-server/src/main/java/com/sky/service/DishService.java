de648c8cba8060de60bcf9ea74c09a9e1e734c08
linzhou <1398596334@qq.com>
2026-01-04 16:46:41 +0800

    导入商品浏览功能

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -22,4 +22,11 @@ public interface DishService {
     void updateStatus(Integer status, Long id);
 
     List<Dish> getDsihByCategoryId(Long categoryId);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
